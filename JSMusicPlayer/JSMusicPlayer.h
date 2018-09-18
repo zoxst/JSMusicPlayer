54188014739b4731a30c28c5297dbd0aa1ab03fd
Jovan Stankovic <zoxst@yahoo.com>
2018-09-18 21:09:40 +0200

    Initial Commit

---
+++ JSMusicPlayer/JSMusicPlayer.h
@@ -0,0 +1,19 @@
+//
+//  JSMusicPlayer.h
+//  JSMusicPlayer
+//
+//  Created by Jovan Stankovic on 9/18/18.
+//  Copyright © 2018 Geoinformatika DOO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSMusicPlayer.
+FOUNDATION_EXPORT double JSMusicPlayerVersionNumber;
+
+//! Project version string for JSMusicPlayer.
+FOUNDATION_EXPORT const unsigned char JSMusicPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSMusicPlayer/PublicHeader.h>
+
+
